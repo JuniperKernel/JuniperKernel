be66a4dd79470088b504acf11cb5686d108bad61
SpencerA <spnrpa@gmail.com>
2017-08-23 18:11:44 -0700

    cleanup code switch to const pointers for sockets & context

--- src/juniper.cpp
+++ src/juniper.cpp
@@ -3,48 +3,9 @@
 #include <zmq.hpp>
 #include <fstream>
 #include <json.hpp>
-
-#define LINGER 1000
-
-#ifndef FINALIZERS_H
-#define FINALIZERS_H
-typedef void(*finalizerT)(SEXP);
-template<typename T>
-SEXP createExternalPointer(T* p, finalizerT finalizer, const char* pname) {
-  SEXP _p;
-  _p = Rcpp::Shield<SEXP>(R_MakeExternalPtr(reinterpret_cast<void*>(p),Rf_install(pname),R_NilValue));
-  R_RegisterCFinalizerEx(_p, finalizer, TRUE);
-  return _p;
-}
-#endif // FINALIZERS_H
+#include <juniper.hpp>
 
 using nlohmann::json;
-
-struct config {
-  std::string control_port;
-  std::string hb_port;
-  std::string iopub_port;
-  std::string ip;
-  std::string key;
-  std::string shell_port;
-  std::string signature_scheme;
-  std::string stdin_port;
-  std::string transport;
-};
-
-// sample config:
-//    { 
-//      "control_port":54596
-//    , "hb_port":54597
-//    , "iopub_port":54594
-//    , "ip":"127.0.0.1"
-//    , "key":"8ac43620-2a4b-477a-8353-ea1eed1e41c2"
-//    , "shell_port":54593
-//    , "signature_scheme":"hmac-sha256"
-//    , "stdin_port":54595
-//    , "transport":"tcp"
-//    }
-
 
 class JuniperKernel {
   public:
@@ -52,45 +13,45 @@ class JuniperKernel {
       std::ifstream ifs(connection_file);
       json connection_info = json::parse(ifs);
       config conf = {
-        connection_info["control_port"].get<std::string>(),
-        connection_info["hb_port"].get<std::string>(),
-        connection_info["iopub_port"].get<std::string>(),
-        connection_info["ip"].get<std::string>(),
-        connection_info["key"].get<std::string>(),
-        connection_info["shell_port"].get<std::string>(),
+        connection_info["control_port"    ].get<std::string>(),
+        connection_info["hb_port"         ].get<std::string>(),
+        connection_info["iopub_port"      ].get<std::string>(),
+        connection_info["ip"              ].get<std::string>(),
+        connection_info["key"             ].get<std::string>(),
+        connection_info["shell_port"      ].get<std::string>(),
         connection_info["signature_scheme"].get<std::string>(),
-        connection_info["stdin_port"].get<std::string>(),
-        connection_info["transport"].get<std::string>(),
+        connection_info["stdin_port"      ].get<std::string>(),
+        connection_info["transport"       ].get<std::string>(),
       };
       return new JuniperKernel(conf);
     }
 
     JuniperKernel(const config& conf)
       : _ctx(new zmq::context_t(1)),
-        _shell(*_ctx, zmq::socket_type::router),
-        _stdin(*_ctx, zmq::socket_type::router),
-        _cntrl(*_ctx, zmq::socket_type::router),
-        _iopub(*_ctx, zmq::socket_type::pub   ),
-        _hbeat(*_ctx, zmq::socket_type::rep   )
+        _shell(new zmq::socket_t(*_ctx, zmq::socket_type::router)),
+        _stdin(new zmq::socket_t(*_ctx, zmq::socket_type::router)),
+        _cntrl(new zmq::socket_t(*_ctx, zmq::socket_type::router)),
+        _iopub(new zmq::socket_t(*_ctx, zmq::socket_type::pub   )),
+        _hbeat(new zmq::socket_t(*_ctx, zmq::socket_type::rep   ))
     {
       char sep = (conf.transport=="tcp") ? ':' : '-';
       std::string endpoint = conf.transport + "://" + conf.ip + sep;
       
-      // setup all of the sockets
-      _shell.setsockopt(ZMQ_LINGER, LINGER);
-      _shell.bind(endpoint + conf.shell_port);
+      // socet setup
+      _shell->setsockopt(ZMQ_LINGER, LINGER);
+      _shell->bind(endpoint + conf.shell_port);
       
-      _stdin.setsockopt(ZMQ_LINGER, LINGER);
-      _stdin.bind(endpoint + conf.stdin_port);
+      _stdin->setsockopt(ZMQ_LINGER, LINGER);
+      _stdin->bind(endpoint + conf.stdin_port);
       
-      _cntrl.setsockopt(ZMQ_LINGER, LINGER);
-      _cntrl.bind(endpoint + conf.control_port);
+      _cntrl->setsockopt(ZMQ_LINGER, LINGER);
+      _cntrl->bind(endpoint + conf.control_port);
       
-      _iopub.setsockopt(ZMQ_LINGER, LINGER);
-      _iopub.bind(endpoint + conf.iopub_port);
+      _iopub->setsockopt(ZMQ_LINGER, LINGER);
+      _iopub->bind(endpoint + conf.iopub_port);
       
-      _hbeat.setsockopt(ZMQ_LINGER, LINGER);
-      _hbeat.bind(endpoint + conf.hb_port);
+      _hbeat->setsockopt(ZMQ_LINGER, LINGER);
+      _hbeat->bind(endpoint + conf.hb_port);
     }
 
     static void finalizer(SEXP jpro) {
@@ -106,22 +67,22 @@ class JuniperKernel {
       // destroy sockets
       // destoy ctx
       if( _ctx ) {
-        _shell.setsockopt(LINGER, 0);
-        _stdin.setsockopt(LINGER, 0);
-        _cntrl.setsockopt(LINGER, 0);
-        _iopub.setsockopt(LINGER, 0);
-        _hbeat.setsockopt(LINGER, 0);
+        _shell->setsockopt(ZMQ_LINGER, 0); delete _shell;
+        _stdin->setsockopt(ZMQ_LINGER, 0); delete _stdin;
+        _cntrl->setsockopt(ZMQ_LINGER, 0); delete _cntrl;
+        _iopub->setsockopt(ZMQ_LINGER, 0); delete _iopub;
+        _hbeat->setsockopt(ZMQ_LINGER, 0); delete _hbeat;
         delete _ctx;
       }
     }
 
   private:
     zmq::context_t* const _ctx;
-    zmq::socket_t _shell;
-    zmq::socket_t _stdin;
-    zmq::socket_t _cntrl;
-    zmq::socket_t _iopub;
-    zmq::socket_t _hbeat;
+    zmq::socket_t* const _shell;
+    zmq::socket_t* const _stdin;
+    zmq::socket_t* const _cntrl;
+    zmq::socket_t* const _iopub;
+    zmq::socket_t* const _hbeat;
 };
 
 // [[Rcpp::export]]
---
+++ src/juniper.hpp
@@ -0,0 +1,40 @@
+#include <string>
+
+#define LINGER 1000
+
+#ifndef FINALIZERS_H
+#define FINALIZERS_H
+typedef void(*finalizerT)(SEXP);
+template<typename T>
+SEXP createExternalPointer(T* p, finalizerT finalizer, const char* pname) {
+  SEXP _p;
+  _p = Rcpp::Shield<SEXP>(R_MakeExternalPtr(reinterpret_cast<void*>(p),Rf_install(pname),R_NilValue));
+  R_RegisterCFinalizerEx(_p, finalizer, TRUE);
+  return _p;
+}
+#endif // FINALIZERS_H
+
+
+// sample config:
+//    {
+//      "control_port":54596
+//    , "hb_port":54597
+//    , "iopub_port":54594
+//    , "ip":"127.0.0.1"
+//    , "key":"8ac43620-2a4b-477a-8353-ea1eed1e41c2"
+//    , "shell_port":54593
+//    , "signature_scheme":"hmac-sha256"
+//    , "stdin_port":54595
+//    , "transport":"tcp"
+//    }
+struct config {
+  std::string control_port;
+  std::string hb_port;
+  std::string iopub_port;
+  std::string ip;
+  std::string key;
+  std::string shell_port;
+  std::string signature_scheme;
+  std::string stdin_port;
+  std::string transport;
+};
