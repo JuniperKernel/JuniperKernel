93399db57a8d2a95beeeac950a5cbd7b006716b3
spencer <saiello8@bloomberg.net>
2017-10-03 22:23:06 +0100

    get a clean windows build

---
+++ chk.bat
@@ -0,0 +1,12 @@
+Rscript -e Rcpp::compileAttributes()
+if %ERRORLEVEL% EQU 1 exit 1
+
+Rscript -e roxygen2::roxygenize()
+if %ERRORLEVEL% EQU 1 exit 1
+
+mkdir build
+R CMD build .
+mv Juniper*tar.gz build
+
+cd build && R CMD check --as-cran --no-manual *tar.gz
+if %ERRORLEVEL% EQU 1 exit 1
