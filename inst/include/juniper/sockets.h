a40799021001c45593d72266ae4ab414c4397467
spencer <spnrpa@gmail.com>
2017-10-26 11:25:40 -0700

    cosmetics

--- inst/include/juniper/sockets.h
+++ inst/include/juniper/sockets.h
@@ -30,26 +30,24 @@ zmq::socket_t* listen_on(zmq::context_t& context, const std::string& endpoint, z
 // false, stop polling and teardown.
 // All callers get an additional signal listener.
 void poll(zmq::context_t& context, zmq::socket_t* sockets[], std::function<bool()> handlers[], int n) {
-
   zmq::pollitem_t items[n + 1 /* one more for the inproc signaller*/];
   zmq::socket_t* signaller = subscribe_to(context, INPROC_SIG);
   items[0] = {*signaller, 0, ZMQ_POLLIN, 0 };
-  for(int i=1; i<=n; i++)
+  for( int i=1; i<=n; i++ )
     items[i] = {*sockets[i-1], 0, ZMQ_POLLIN, 0};
 
   bool dead=false;
   while( !dead ) {
     zmq::poll(items, n+1, -1);
     std::function<bool()>* hp = handlers;
-    if( items[0].revents & ZMQ_POLLIN ) {  // got a kill signal
-      dead=true;
+    if( (dead=(items[0].revents & ZMQ_POLLIN)) )  // got a kill signal
       break;
-    }
+
     try {
-    for(int i=1; i<=n; i++)
-      if( items[i].revents & ZMQ_POLLIN )
-        if( (dead=!hp[i-1]()) )
-          break;
+      for( int i=1; i<=n; i++ )
+        if( items[i].revents & ZMQ_POLLIN )
+          if( (dead=!hp[i-1]()) )
+            break;
     } catch(const std::exception& x) {
       std::cout << "Encountered C++ exception: " <<  x.what() << std::endl;
       std::cout << "Polling continues..." << std::endl;
@@ -57,7 +55,7 @@ void poll(zmq::context_t& context, zmq::socket_t* sockets[], std::function<bool(
   }
 
   assert(dead);
-  
+
   // cleanup
   // loop over sockets, set their lingers to 0, and delete
   signaller->setsockopt(ZMQ_LINGER, 0);
