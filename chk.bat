4eceddfec6a30a6f62bde5a005ab105fb0984440
spencer <spnrpa@gmail.com>
2017-12-01 16:20:02 -0800

    start building test infra

--- chk.bat
+++ chk.bat
@@ -1,3 +1,6 @@
+make clean
+if %ERRORLEVEL% EQU 1 exit 1
+
 Rscript -e Rcpp::compileAttributes()
 if %ERRORLEVEL% EQU 1 exit 1
 
@@ -8,5 +11,7 @@ mkdir build
 R CMD build .
 mv Juniper*tar.gz build
 
-cd build && R CMD check --as-cran *tar.gz
+R CMD check --no-manual --no-build-vignettes --as-cran build\*tar.gz
 if %ERRORLEVEL% EQU 1 exit 1
+
+R -f scripts\r_cmd_check_validate.R
---
+++ test/r_cmd_check_validate.R
@@ -0,0 +1,127 @@
+# This script checks the output of a R CMD CHECK run
+# This is run as part of the CI suite of tests for JuniperKernel.
+#
+# This step performs the following steps:
+#   1. Read in the file JuniperKernel.Rcheck/00check.log
+#   2. For each "check" (line beginning with "* chceking"), grep for status
+#   3. The status may be one of [NOTE, WARNING, ERROR, OK]
+#   4. Sometimes NOTE is ok, so check it against a list of allowed notes
+#   5. WARNING status anywhere is an invalid R CMD CHECK
+#   6. If R CMD CHECK has a status of FAIL ERROR or WARN, then it is invalid
+#
+# Lines look like this:
+#     * checking R files for non-ASCII characters ... WARNING
+#     * checking for code/documentation mismatches ... OK
+#
+#   On Windows, i386 and x64 architectures are checked:
+#     * loading checks for arch 'i386'
+#     ** checking whether the package can be loaded ... OK
+#     ...
+#     * loading checks for arch 'x64'
+#     ** checking whether the package can be loaded ... OK
+#     ...
+#
+# Luckily, the `tools` package has some handy unexported functionality
+# that is used to preprocess this log file!
+(function() {
+  path <- file.path('.', 'JuniperKernel.Rcheck', '00check.log')
+  if( !file.exists(path) )
+    stop("00check.log not found.")
+
+  chkResults <- tools:::check_packages_in_dir_results(logs=path)
+  if( is.null(chkResults) || length(chkResults)==0L )
+    stop("Invalid 00check.log")
+
+  if( chkResults$JuniperKernel$status %in% c("FAIL", "ERROR", "WARN") )
+    stop("unimpl")
+
+  stop("unimpl")
+})()
+
+# # NOTE whitelist to be used by grepl, for each check step
+# whitelist = list(
+#   "CRAN incoming feasibility" = c("Maintainer:",
+#                                   "Checking URLs requires 'libcurl' support in the R build",
+#                                   "Package has FOSS license, installs .class/.jar but has no 'java' directory",
+#                                   "Insufficient package version", # when we check older version of package than current available on CRAN
+#                                   "Days since last update", # when we submit to CRAN recently
+#                                   "Number of updates in past 6 months"), # when we submit to CRAN too often
+#   "package dependencies" = c("No repository set, so cyclic dependency check skipped",
+#                              "Package suggested but not available for checking"),
+#   "installed package size" = c("installed size is .*Mb", # h2o.jar is installed
+#                                "sub-directories of 1Mb or more",
+#                                "java .*Mb"),
+#   "Rd cross-references" = "Package unavailable to check Rd xrefs" # when linking documentation to optional deps not present at check time
+# )
+# check_note <- function(details, whitelist, verbose=TRUE) {
+#   stopifnot(is.data.frame(details), is.list(whitelist))
+#   status_details <- details[details$Status=="NOTE", , drop=FALSE]
+#   if (!nrow(status_details))
+#     stop("No NOTEs were found in provided details, be sure to escape this call when no NOTEs present")
+#   lines <- strsplit(status_details$Output, "\n", fixed=TRUE)
+#   names(lines) <- status_details$Check
+#   filter_check <- function(check, lines, whitelist) {
+#     check_lines <- lines[[check]]
+#     check_lines <- check_lines[nzchar(check_lines)]
+#     check_whitelist <- whitelist[[check]]
+#     # no whitelist for this check step, all notes are valid
+#     if (!length(check_whitelist))
+#       return(check_lines)
+#     # check if whitelisted
+#     wl <- sapply(
+#       check_lines,
+#       function(line) any(sapply(
+#         check_whitelist,
+#         function (pattern) grepl(pattern, line)
+#         ))
+#     )
+#     check_lines[!wl]
+#   }
+#   new_note <- sapply(names(lines), filter_check, lines, whitelist, simplify=FALSE)
+#   new_note_row <- which(sapply(new_note, length) > 0L)
+#   new_note <- new_note[new_note_row]
+#   new_note_flags <- unique(status_details[new_note_row, "Flags"])
+#
+#   ans <- !length(new_note)
+#   if (verbose && !ans) {
+#     new_note_body <- unlist(lapply(names(new_note), function(check) {
+#       c(paste("* checking", check), new_note[[check]])
+#     }))
+#     message(sprintf("There %s %s unexpected NOTE%s in R CMD check%s\n\n%s",
+#                     if(length(new_note)>1L) "are" else "is",
+#                     length(new_note),
+#                     if(length(new_note)>1L) "s" else "",
+#                     paste(c("",new_note_flags), collapse=" "),
+#                     paste(new_note_body, collapse="\n")))
+#   }
+#   ans
+# }
+#
+# pkg <- if (length(args <- commandArgs(trailingOnly=TRUE))) args[[1]]
+# rcheck_dir <- Sys.glob(if (is.null(pkg)) "*.Rcheck" else paste(pkg, ".Rcheck", sep=""))
+# if (length(rcheck_dir)!=1L) stop("This script should be started from directory in which *.Rcheck directory is located, and there must be single Rcheck directory.")
+#
+# log_file <- file.path(rcheck_dir, "00check.log")
+# res <- tools:::check_packages_in_dir_results(logs = log_file)[[1L]]
+# if (length(res)) {
+#   if (res$status %in% c("FAIL","ERROR","WARN")) {
+#     status <- 1
+#     meta <- tools:::analyze_check_log(log_file)
+#     # print whole file
+#     message(sprintf("R CMD check%s completed with %s\n\n%s",
+#                     paste(c("",meta[["Flags"]]), collapse=" "),
+#                     res$status,
+#                     paste(readLines(log_file, warn=FALSE), collapse="\n")))
+#   } else if (res$status=="NOTE") {
+#     details <- tools:::check_packages_in_dir_details(logs = log_file)
+#     # print notes which are not whitelisted
+#     note_ok <- check_note(details, whitelist)
+#     status <- if (note_ok) 0 else 1
+#   } else if (res$status=="OK") {
+#     status <- 0
+#   } else stop("Unknown status from R CMD check: ", toString(res$status))
+# } else stop("Unknown results from R CMD check")
+#
+# if (status==0) cat(sprintf("INFO: %s package R CMD check output successfully validated.\n", sub("\\.Rcheck$", "", rcheck_dir)))
+#
+# if (!interactive()) q("no", status=status)
