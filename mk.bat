c83d86117720bb70ae5225fa3fb2b080afcf2e8d
spennihana <spnrpa@gmail.com>
2017-12-25 18:45:00 -0800

    Revert "Add repo"
    This reverts commit dcfc434d20c7df8b8123a2f82a2c13be24e2019a.

--- mk.bat
+++ mk.bat
@@ -46,9 +46,6 @@ if %ERRORLEVEL% EQU 1 exit 1
 Rscript -e roxygen2::roxygenize()
 if %ERRORLEVEL% EQU 1 exit 1
 
-Rscript -e "install.packages('devtools',repo='https://cran.r-project.org')"
-if %ERRORLEVEL% EQU 1 exit 1
-
 Rscript -e "devtools::install_github('snoweye/pbdZMQ')"
 if %ERRORLEVEL% EQU 1 exit 1
 
