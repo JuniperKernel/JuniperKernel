2868f1cdc6566caf2f31364f73cbda7653e1e734
wccsnow <wccsnow@gmail.com>
2018-01-27 20:00:11 -0500

    Simplify zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -17,47 +17,10 @@
 
 .onAttach <- function(lib, pkg) {}
 
-.onLoad <- function(libname, pkgname){
-  ### For osx only.
-  if(Sys.info()[['sysname']] == "Darwin"){
-    cmd.ot <- system("which otool", intern = TRUE)
-
-    ### Get rpath from pkgname's shared library.
-    fn.so <- paste(libname, "/", pkgname, "/libs/", pkgname, ".so", sep = "")
-    rpath <- system(paste(cmd.ot, " -L ", fn.so, sep = ""), intern = TRUE)
-
-    ### Get the installed dylib from pkgname's rpath.
-    pattern <- paste("^\\t(.*/pbdZMQ/libs/libzmq.*\\.dylib) .*$", sep = "")
-    i.rpath <- grep(pattern, rpath)
-    fn.dylib <- gsub(pattern, "\\1", rpath[i.rpath])
-    if(length(fn.dylib) == 1){
-
-      ### The file of the installed dylib does not exist.
-      if(!file.exists(fn.dylib)){
-        cmd.int <- system("which install_name_tool", intern = TRUE)
-
-        ### Search the dylib from R's library path.
-        dn <- tools::file_path_as_absolute(
-                system.file("./libs", package = "pbdZMQ")) 
-        fn <- list.files(path = dn, pattern = "libzmq.*\\.dylib")
-
-        ### Install the searched one to the pkgname's shared library.
-        if(length(fn) == 1){
-          new.fn.dylib <- paste(dn, "/", fn, sep = "")
-          cmd <- paste(cmd.int, " -change ", fn.dylib, " ", new.fn.dylib,
-                       " ", fn.so,
-                       sep = "")
-          system(cmd)
-        } else{
-          stop("Neither external nor internal ZMQ can be found.") 
-        }
-      }
-    }
-  }
-
-  ### Load "pkgname.so".
+.onLoad <- function(lib, pkg) {
+  pbdZMQ::overwrite.shpkg.rpath(mylib = lib, mypkg = pkg, linkingto = "pbdZMQ",
+                                shlib = "zmq")
   library.dynam("JuniperKernel", pkgname, libname)
-
   invisible()
 } # End of .onLoad().
 
