b71fe48cca23aede09325985ff0d50950b1ee92e
spennihana <spnrpa@gmail.com>
2018-08-09 20:23:37 -0700

    install 5.0.1 of roxygen2

--- mk.bat
+++ mk.bat
@@ -30,10 +30,14 @@ C:\Rtools\bin\cp .\inst\nl_json.hpp .\inst\include\xeus\nl_json.hpp
 Rscript -e Rcpp::compileAttributes()
 if %ERRORLEVEL% EQU 1 exit 1
 
-Rscript -e roxygen2::roxygenize()
+Rscript -e "install.packages('devtools',repo='https://cran.r-project.org')"
 if %ERRORLEVEL% EQU 1 exit 1
 
-Rscript -e "install.packages('devtools',repo='https://cran.r-project.org')"
+:: install a version of roxygen2 that does not attempt to build the package...
+Rscript -e "devtools::install_version('roxygen2', version='5.0.1', repo='https://cran.r-project.org')"
+if %ERRORLEVEL% EQU 1 exit 1
+
+Rscript -e roxygen2::roxygenize()
 if %ERRORLEVEL% EQU 1 exit 1
 
 Rscript -e "install.packages('subprocess',repo='https://cran.r-project.org')"
