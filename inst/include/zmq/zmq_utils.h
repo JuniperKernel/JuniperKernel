01cd75080b438419d5f8339deaf8e5dcea4e3805
spennihana <spnrpa@gmail.com>
2018-09-18 01:21:07 -0700

    comment out pragmas

--- inst/include/zmq/zmq_utils.h
+++ inst/include/zmq/zmq_utils.h
@@ -37,14 +37,14 @@
 #if defined(__clang__) || defined(__GNUC__) || defined(__GNUG__)               \
   || defined(_MSC_VER)
 #if defined(__GNUC__) || defined(__GNUG__)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic warning "-Wcpp"
-#pragma GCC diagnostic ignored "-Werror"
-#pragma GCC diagnostic ignored "-Wall"
+// #pragma GCC diagnostic push
+// #pragma GCC diagnostic warning "-Wcpp"
+//#pragma GCC diagnostic ignored "-Werror"
+//#pragma GCC diagnostic ignored "-Wall"
 #endif
 #pragma message(                                                               \
   "Warning: zmq_utils.h is deprecated. All its functionality is provided by zmq.h.")
 #if defined(__GNUC__) || defined(__GNUG__)
-#pragma GCC diagnostic pop
+//#pragma GCC diagnostic pop
 #endif
 #endif
